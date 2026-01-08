7ac026f9ae45b4cfd89d5f79a013258feca1d2b2
michiel1106 <michielimmink@gmail.com>
2026-01-08 18:11:06 +0100

    pig

--- src/client/java/bikerboys/cuteanimalsport/registry/ModModelLayers.java
+++ src/client/java/bikerboys/cuteanimalsport/registry/ModModelLayers.java
@@ -6,6 +6,7 @@ import net.minecraft.resources.*;
 
 public class ModModelLayers {
     public static final ModelLayerLocation BABY_OCELOT = new ModelLayerLocation(Identifier.fromNamespaceAndPath(Constants.MOD_ID, "baby_ocelot"), "main");
+    public static final ModelLayerLocation BABY_PIG = new ModelLayerLocation(Identifier.fromNamespaceAndPath(Constants.MOD_ID, "baby_pig"), "main");
 
 
     public static void register() {
--- src/client/java/bikerboys/cuteanimalsport/registry/Models.java
+++ src/client/java/bikerboys/cuteanimalsport/registry/Models.java
@@ -2,6 +2,7 @@ package bikerboys.cuteanimalsport.registry;
 
 import bikerboys.cuteanimalsport.models.base.*;
 import bikerboys.cuteanimalsport.models.ocelot.*;
+import bikerboys.cuteanimalsport.models.pig.*;
 import net.fabricmc.fabric.api.client.rendering.v1.*;
 
 public class Models {
@@ -9,6 +10,7 @@ public class Models {
     public static void register() {
 
         EntityModelLayerRegistry.registerModelLayer(ModModelLayers.BABY_OCELOT, BabyOcelotModel::createBabyLayer);
+        EntityModelLayerRegistry.registerModelLayer(ModModelLayers.BABY_PIG, BabyPigModel::createBodyLayer);
 
     }
 
