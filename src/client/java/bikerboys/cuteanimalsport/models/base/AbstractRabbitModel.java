22acae7ef2b75bec655430a3afe058ead3b8f6d3
michiel1106 <michielimmink@gmail.com>
2026-01-10 22:17:26 +0100

    model + texture of rabbit

--- src/client/java/bikerboys/cuteanimalsport/models/base/AbstractRabbitModel.java
+++ src/client/java/bikerboys/cuteanimalsport/models/base/AbstractRabbitModel.java
@@ -6,6 +6,7 @@ import net.minecraft.client.animation.KeyframeAnimation;
 import net.minecraft.client.model.EntityModel;
 import net.minecraft.client.model.geom.ModelPart;
 import net.minecraft.client.renderer.entity.state.RabbitRenderState;
+import net.minecraft.util.*;
 
 public abstract class AbstractRabbitModel extends RabbitModel {
     protected static final String FRONT_LEGS = "frontlegs";
@@ -21,13 +22,30 @@ public abstract class AbstractRabbitModel extends RabbitModel {
         this.idleHeadTiltAnimation = idleHeadTilt.bake(root);
     }
 
-    public void setupAnim(final RabbitRenderState state) {
-        super.setupAnim(state);
+    public void setupAnim(final RabbitRenderState rabbitRenderState) {
+        super.setupAnim(rabbitRenderState);
+
+        ModelPart body = getChildPart("body");
+
+        ModelPart head = body.getChild("head");
+        ModelPart tail = body.getChild("tail");
+
+        ModelPart frontLegs = body.getChild("frontlegs");
+        ModelPart rightFrontLeg = frontLegs.getChild("right_front_leg");
+        ModelPart leftFrontLeg = frontLegs.getChild("left_front_leg");
+
+        ModelPart backLegs = getChildPart("backlegs");
+        ModelPart rightHaunch = backLegs.getChild("right_hind_leg").getChild("right_haunch");
+        ModelPart leftHaunch = backLegs.getChild("left_hind_leg").getChild("left_haunch");
 
 
-
-        this.hopAnimation.apply(state.hopAnimationState, state.ageInTicks);
-        this.idleHeadTiltAnimation.apply(state.idleHeadTiltAnimationState, state.ageInTicks);
+        head.xRot = rabbitRenderState.xRot * (float) (Math.PI / 180.0);
+        head.yRot = rabbitRenderState.yRot * (float) (Math.PI / 180.0);
+        float f = Mth.sin(rabbitRenderState.jumpCompletion * (float) Math.PI);
+        leftHaunch.xRot += f * 50.0F * (float) (Math.PI / 180.0);
+        rightHaunch.xRot += f * 50.0F * (float) (Math.PI / 180.0);
+        leftFrontLeg.xRot += f * -40.0F * (float) (Math.PI / 180.0);
+        rightFrontLeg.xRot += f * -40.0F * (float) (Math.PI / 180.0);
 
 
     }
